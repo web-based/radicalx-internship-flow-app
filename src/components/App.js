be9bf3c77cd9e3922399a51305d1f80688faab08
osvaldo <iosvaldo.app@gmail.com>
2022-10-08 18:10:42 -0400

    styled the signup form using Bootstrap 🐸

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
 import Signup from './Signup'
 
 
 function App() {
   return (
-    <Signup/>
-  )
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
