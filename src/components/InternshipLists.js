2c0bfe3ac9b5fa560144d3d298290b5a3ecd4378
Anson Lee <allee11@dons.usfca.edu>
2022-10-11 21:48:58 -0700

    Updated internship list/css 🍞

--- src/components/InternshipLists.js
+++ src/components/InternshipLists.js
@@ -6,18 +6,143 @@ function InternshipLists() {
     <div className="tableLayout">
         <div className="colOne">
             <div className="colOneEle tableHeader">Internship Title</div>
+            <div className = "colOneEle">
+               <div className = "colOneTextBox">
+                    <div className="colOneMainText">1</div>
+                    <div className="colOneText">11</div>
+                </div> 
+            </div>
+            <div className = "colOneEle">
+                <div className = "colOneTextBox">
+                    <div className="colOneMainText">1</div>
+                    <div className="colOneText">11</div>
+                </div> 
+            </div>
+            <div className = "colOneEle">
+                <div className = "colOneTextBox">
+                    <div className="colOneMainText">1</div>
+                    <div className="colOneText">11</div>
+                </div> 
+            </div>
+            <div className = "colOneEle">
+                <div className = "colOneTextBox">
+                    <div className="colOneMainText">1</div>
+                    <div className="colOneText">11</div>
+                </div> 
+            </div>
+            <div className = "colOneEle">
+                <div className = "colOneTextBox">
+                    <div className="colOneMainText">1</div>
+                    <div className="colOneText">11</div>
+                </div> 
+            </div>
         </div>
         <div className="colTwo">
             <div className="colTwoEle tableHeader">Completion Period</div>
+            <div className = "colTwoEle">
+                <div className = "colTwoTextBox">
+                    <div className="colTwoDayText">2</div>
+                    <div className="colTwoText">22</div>
+                </div>
+            </div>
+            <div className = "colTwoEle">
+                <div className = "colTwoTextBox">
+                    <div className="colTwoDayText">2</div>
+                    <div className="colTwoText">22</div>
+                </div>
+            </div>
+            <div className = "colTwoEle">
+                <div className = "colTwoTextBox">
+                    <div className="colTwoDayText">2</div>
+                    <div className="colTwoText">22</div>
+                </div>
+            </div>
+            <div className = "colTwoEle">
+                <div className = "colTwoTextBox">
+                    <div className="colTwoDayText">2</div>
+                    <div className="colTwoText">22</div>
+                </div>
+            </div>
+            <div className = "colTwoEle">
+                <div className = "colTwoTextBox">
+                    <div className="colTwoDayText">2</div>
+                    <div className="colTwoText">22</div>
+                </div>
+            </div>
         </div>
         <div className="colThree">
-            <div className="colTwoEle tableHeader">Total Enrolled</div>
+            <div className="colThreeEle tableHeader">Total Enrolled</div>
+            <div className = "colThreeEle">
+                <div className = "colThreeTextBox">
+                    <div className="colThreeText">3</div>
+                </div>
+            </div>
+            <div className = "colThreeEle">
+                <div className = "colThreeTextBox">
+                    <div className="colThreeText">3</div>
+                </div>
+            </div>
+            <div className = "colThreeEle">
+                <div className = "colThreeTextBox">
+                    <div className="colThreeText">3</div>
+                </div>
+            </div>
+            <div className = "colThreeEle">
+                <div className = "colThreeTextBox">
+                    <div className="colThreeText">3</div>
+                </div>
+            </div>
+            <div className = "colThreeEle">
+                <div className = "colThreeTextBox">
+                    <div className="colThreeText">3</div>
+                </div>
+            </div>
         </div>
         <div className="colFour">
             <div className="colFourEle tableHeader">Qualified Candidates</div>
+            <div className = "colFourEle">
+                <div className="colFourBox">
+                    <div className="colFourTextBox">
+                        <div className="colFourText">4</div>
+                    </div>
+                </div>
+            </div>
+            <div className = "colFourEle">
+                <div className="colFourBox">
+                    <div className="colFourTextBox">
+                        <div className="colFourText">4</div>
+                    </div>
+                </div>
+            </div>
+            <div className = "colFourEle">
+                <div className="colFourBox">
+                    <div className="colFourTextBox">
+                        <div className="colFourText">4</div>
+                    </div>
+                </div>
+            </div>
+            <div className = "colFourEle">
+                <div className="colFourBox">
+                    <div className="colFourTextBox">
+                        <div className="colFourText">4</div>
+                    </div>
+                </div>
+            </div>
+            <div className = "colFourEle">
+                <div className="colFourBox">
+                    <div className="colFourTextBox">
+                        <div className="colFourText">4</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="colFive">
             <div className="colFiveEle tableHeader"></div>
+            <div className = "colFiveEle">5</div>
+            <div className = "colFiveEle">5</div>
+            <div className = "colFiveEle">5</div>
+            <div className = "colFiveEle">5</div>
+            <div className = "colFiveEle">5</div>
         </div>
     </div> 
   );
